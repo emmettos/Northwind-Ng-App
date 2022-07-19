ef87eb5796c3cc10d7066fafac1e5a262dc7e01d
Emmett OSullivan <emmett.j.osullivan@gmail.com>
2022-07-19 11:55:47 +0100

    Added configurable MSAL redirect URL.

--- src/app/auth-config.ts
+++ src/app/auth-config.ts
@@ -1,21 +1,23 @@
+import { environment } from '../environments/environment';
+
 import { LogLevel, Configuration, BrowserCacheLocation } from '@azure/msal-browser';
 
-const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigator.userAgent.indexOf("Trident/") > -1;
+const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
  
 export const b2cPolicies = {
   names: {
-    signUpSignIn: "B2C_1_SIGNUP-SIGNIN",
-    editProfile: "b2c_1_edit_profile_v2"
+    signUpSignIn: 'B2C_1_SIGNUP-SIGNIN',
+    editProfile: 'b2c_1_edit_profile_v2'
   },
   authorities: {
     signUpSignIn: {
-      authority: "https://eosoftwareltd.b2clogin.com/eosoftwareltd.onmicrosoft.com/B2C_1_SIGNUP_SIGNIN",
+      authority: 'https://eosoftwareltd.b2clogin.com/eosoftwareltd.onmicrosoft.com/B2C_1_SIGNUP_SIGNIN',
     },
     editProfile: {
-      authority: "https://your-tenant-name.b2clogin.com/your-tenant-name.onmicrosoft.com/b2c_1_edit_profile_v2"
+      authority: 'https://your-tenant-name.b2clogin.com/your-tenant-name.onmicrosoft.com/b2c_1_edit_profile_v2'
     }
   },
-  authorityDomain: "eosoftwareltd.b2clogin.com"
+  authorityDomain: 'eosoftwareltd.b2clogin.com'
 };
   
 export const MSAL_CONFIG: Configuration = {
@@ -23,7 +25,7 @@ export const MSAL_CONFIG: Configuration = {
     clientId: 'ea6bb630-42e8-415f-84b0-c0ba77de7542',
     authority: b2cPolicies.authorities.signUpSignIn.authority,
     knownAuthorities: [b2cPolicies.authorityDomain],
-    redirectUri: 'http://localhost:4200', 
+    redirectUri: environment.authConfig.redirectURL 
   },
   cache: {
     cacheLocation: BrowserCacheLocation.LocalStorage,
